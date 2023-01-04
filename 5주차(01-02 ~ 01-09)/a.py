beec4915a6d7028d6f318456c46ecf802acd88b4
gkfkktrh153 <gkfktkrh153@naver.com>
2023-01-05 00:28:43 +0900

    24일차
    개미

--- 5주차(01-02 ~ 01-09)/a.py
+++ 5주차(01-02 ~ 01-09)/a.py
@@ -1,3 +1,9 @@
+def swap(i1, i2):
+  global str
+  temp = str[i1]
+  str[i1] = str[i2]
+  str[i2] = temp
+
 N1, N2 = list(map(int, input().split()))
 
 a = list(input())
@@ -5,11 +11,33 @@ b = list(input())
 
 T = int(input())
 
-a.sort(reverse = True)
-
+a.reverse()
 str = a + b
-print(str)
 
-#for i in range(T):
+for t in range(T):
+  index = []
+  for k in range(len(str) - 1):
+    if (str[k] in a and str[k+1] in b):
+      index.append(k)
+  for i in index:
+    swap(i, i + 1)
   
-  
+
+print(''.join(str))
+
+
+"""
+A - 1234  B - 567
+
+1235467
+
+1253647
+
+1526374
+
+5162734
+
+5617234
+
+
+"""
